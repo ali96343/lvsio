fa0c57c6a927ef372d0fcf3d65d5c92254cdb0ec
AliBsk <ab96343@gmail.com>
2022-03-29 15:22:14 +0300

    +ping_pong_url

--- ssep4w/sse-emit-ping.py
+++ ssep4w/sse-emit-ping.py
@@ -3,8 +3,19 @@
 import time
 import requests
 
+import os
+this_dir = os.path.dirname(os.path.abspath(__file__)).split('/')
+APP_NAME = this_dir[-1]
+
+
+ping_url = 'http://localhost:8000/%s/ping' % APP_NAME
+
+print ('ping_url: ', ping_url )
+
+
+
 while True:
-    requests.get('http://localhost:8000/ssep4w/ping')
+    requests.get(ping_url)
     time.sleep(1)
 
 
