3ea4161dde7e60bdc1f38b2de615b63faf4ac156
AliBsk <ab96343@gmail.com>
2022-07-27 13:21:07 +0300

    fix2 l_menu

--- mymenu/left_menu.py
+++ mymenu/left_menu.py
@@ -1,29 +1,32 @@
 from py4web import URL
 from .settings import APP_NAME
 
-ctrl_pref = f"{APP_NAME}/"
+# the menu navigation file is borrowed from web2py
+
+app_url = f"{APP_NAME}/"
 
 def nav_item(item_nm, ctrl_nm, flag= False):
-    return item_nm, flag, URL( ctrl_pref  + ctrl_nm)
+    return item_nm, flag, URL( app_url + ctrl_nm )
 
 
 l_menu = [
-    ("Home", False, URL(ctrl_pref), []),
+    ("Home", False, URL(app_url), []),
 
     ( "Func", False, "#", [
-            ("mi1", False, URL(ctrl_pref + "mi1")),
-            ("upload", False, URL(ctrl_pref )),
-            ("tlist", False, URL(ctrl_pref )),
+            ("mi1", False, URL(app_url + "mi1")),
+            ("upload", False, URL(app_url )),
+            ("tlist", False, URL(app_url )),
         ],
     ),
 
     ( "Demo", False, "#", [
-            ("mi2", False, URL(ctrl_pref + "mi2")),
-            ("index", False, URL(ctrl_pref )),
-            ("mi3", False, URL(ctrl_pref + "mi3")),
+            nav_item("mi2", "mi2"),
+            ("index", False, URL(app_url )),
+            ("mi3", False, URL(app_url + "mi3")),
             nav_item("yyy", "mi4"),
         ],
     ),
+
     ( "Setup", False, "#", [
             nav_item("x", "mi1"),
             nav_item("y", "mi2"),
